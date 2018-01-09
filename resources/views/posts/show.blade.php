e37bb6ed281709160996ac4bcdc2a2a2e4351cae
Lester G <Lester G>
2018-01-09 11:45:13 +0800

    part 15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,11 +2,29 @@
 
 @section ('content')
     <div class="col-sm-8">
-        <h2 class="blog-post-title"><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h2>
+        <h2 class="blog-post-title">{{ $post->title }}</h2>
         <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString()}}
             <!-- by<a href="#">Chris</a></p> -->
 
         <p>{{ $post->body }}</p>
+
+        <hr />
+
+        <div class="comments">
+            <ul class="list-group">
+                @foreach ($post->comments as $comment)
+                    <li class="list-group-item">
+                        <strong>
+                            {{$comment->created_at->diffForHumans()}}:&nbsp;
+                        </strong>
+
+                        {{$comment->body}}
+                    </li>
+
+                @endforeach
+            </ul>
+
+        </div>
     </div>
 
 @endsection
